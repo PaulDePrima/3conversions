90d07d61515df58362e7fef99ba4a3a84cb9f6ca
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-10-10 09:59:29 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,15 +3,13 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
-def render_main():
-    if "color" in request.args:
-        celcius = ((float(request.args['color']))-32)*5/9 
-        return render_template('home.html', response = "the temp in celcius is" + celcius)
-    else:
-         return render_template('home.html')
+def render_main()
+    return render_template('home.html')
 
 @app.route("/p1")
 def render_page1():
+    celcius = ((float(request.args['color']))-32)*5/9 
+    return render_template('home.html', response = "the temp in celcius is" + celcius)
     return render_template('page1.html')
 
 @app.route("/p2")
