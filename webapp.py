527a397a5fc8b5da83df07f612231029526bf8ba
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-10-12 08:54:26 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,6 +11,17 @@ def render_page1():
     celcius = ((float(request.args['color']))-32)*5/9 
     return render_template('home.html', response = "the temp in celcius is" + celcius)
 
+@app.route("/response")
+def render_response():
+    color = request.args['color']
+    #The request object stores information about the request sent to the server.
+    #args is a MultiDict (like a dictionary but can have muiltiple values for the same key)
+    if color == 'purple':
+        reply = "That's my favorite color, too!"
+    else:
+        reply = "My favorite color is purple."
+    return render_template('response.html', response = reply)
+
 @app.route("/p2")
 def render_page2():
      return render_template('page2.html')
