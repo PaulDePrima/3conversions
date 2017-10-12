bad7f2c6410f940ca3e313f70c30988b58a6abec
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-10-12 09:57:42 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,7 +6,7 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main()
     return render_template('home.html')
 
-@app.route("/p1")
+"""@app.route("/p1")
 def render_page1():
     celcius = ((float(request.args['color']))-32)*5/9 
     return render_template('home.html', response = "the temp in celcius is" + celcius)
@@ -22,6 +22,8 @@ def render_response():
 @app.route("/p2")
 def render_page2():
      return render_template('page2.html')
+     
+"""
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
