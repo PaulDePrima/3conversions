1f8f1603647007a83ae5c57a04bb068a4641b450
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-10-12 09:44:29 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -13,14 +13,11 @@ def render_page1():
 
 @app.route("/response")
 def render_response():
-    color = request.args['color']
+    far = request.args['color']
+    res = str((far-32)*5/9)
     #The request object stores information about the request sent to the server.
-    #args is a MultiDict (like a dictionary but can have muiltiple values for the same key)
-    if color == 'purple':
-        reply = "That's my favorite color, too!"
-    else:
-        reply = "My favorite color is purple."
-    return render_template('response.html', response = reply)
+    #args is a MultiDict (like a dictionary but can have muiltiple values for the same key
+    return render_template('response.html',their = request.args['color'], response = res)
 
 @app.route("/p2")
 def render_page2():
